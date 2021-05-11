0d333251fef8d127937e90714c387ae7f04dddd3
Marius Tudorie <tudorie.mariuscosmin@gmail.com>
2021-05-11 15:20:28 +0300

    implementing Command

---
+++ week_11_Chain_Command/src/ro/ase/csie/cts/g1094/dp/command/AsyncTask.java
@@ -0,0 +1,18 @@
+package ro.ase.csie.cts.g1094.dp.command;
+
+public class AsyncTask implements AsyncTaskInterface{
+    String taskName;
+    int priority;
+    GameModuleInterface module;
+
+    public AsyncTask(String taskName, int priority, GameModuleInterface module) {
+        this.taskName = taskName;
+        this.priority = priority;
+        this.module = module;
+    }
+
+    @Override
+    public void awaitTask() {
+       this.module.doTask(taskName);
+    }
+}
